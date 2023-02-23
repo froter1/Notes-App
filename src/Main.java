0f2b0d2a81156ab7ddedf50406db6efa4843ab3d
froter1 <fryderyks@onet.pl>
2023-02-23 02:43:17 +0100

    Introducing structure for log in

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+    public static boolean isLogged = false;
+    protected final String username;
+
+
+    public Main(String username) {
+        this.username = username;
+    }
+
+    public static void main(String[] args) {
+        String arg;
+
+        while (!isLogged) {
+            arg = input.next();
+
+            switch (arg) {
+                case "login":
+                    if (Commands.login()) {
+                        Main user = new Main(LoginHandling.username);
+
+                        System.out.println(user.username);
+                    }
+            }
+        }
+    }
+}
